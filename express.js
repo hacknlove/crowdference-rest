906400a86dfa71532a5c4840001aa8e7d7715081
antonio <antonio@pykiss.com>
2019-06-03 15:13:00 +0200

    remove cors

--- express.js
+++ express.js
@@ -5,7 +5,7 @@ module.exports = function (server, port, ip) {
   server.app = require('express')()
   server.app.enable('trust proxy')
   server.app.use(require('compression')())
-  server.app.use(require('cors')())
+  // server.app.use(require('cors')())
   server.app.use(bodyParser.json())
   server.app.use(bodyParser.raw())
   server.app.use(bodyParser.text())
