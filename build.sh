40c6ad96b93fab3d24a92abd5e303d27fa4d4f7a
antonio <antonio@pykiss.com>
2019-06-06 09:01:40 +0200

    better build

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ NAME=$(node -pe "require('./package.json').name")
 docker build --tag hacknlove/$NAME:$VERSION ./
 
 docker push hacknlove/$NAME:$VERSION
+docker tag hacknlove/$NAME:$VERSION hacknlove/$NAME:latest
+docker push hacknlove/$NAME:latest
